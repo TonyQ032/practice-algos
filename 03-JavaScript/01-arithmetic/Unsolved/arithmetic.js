b6d2ad0b76933f86418bef620753c4b1279b5350
aq032 <anthonyq032@gmail.com>
2022-11-24 14:35:35 -0800

    Solved 01-arithmetic

--- 03-JavaScript/01-arithmetic/Unsolved/arithmetic.js
+++ 03-JavaScript/01-arithmetic/Unsolved/arithmetic.js
@@ -1,9 +1,21 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {};
+// Super simple adding function. Returns the sum of num1 and num2
+const add = (num1, num2) => {
+  return num1 + num2;
+};
 
-var subtract = function(num1, num2) {};
+// Super simple subtraction function. Returns the difference of num1 and num2
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
 
-var multiply = function(num1, num2) {};
+// Super simple multiplication function. Returns the product of num1 and num2
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
 
-var divide = function(num1, num2) {};
+// Super simple division function. Returns the quotient of num1 and num2
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
