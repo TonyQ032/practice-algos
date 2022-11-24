f90bdaa9757ef3a6ae36b9182a19a526bbfbb740
aq032 <anthonyq032@gmail.com>
2022-11-24 15:19:36 -0800

    Solved log-nums

--- 03-JavaScript/02-odd-or-even/Unsolved/odd-or-even.js
+++ 03-JavaScript/02-odd-or-even/Unsolved/odd-or-even.js
@@ -1,7 +1,9 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
 
-var oddOrEven = function (num) {
+const oddOrEven = (num) => {
+
+  // Checks if the number has any remainders when divided by 2. If it is even then there should never be a remainder.
   if (num % 2 === 0) {
     return "even";
   } else {
