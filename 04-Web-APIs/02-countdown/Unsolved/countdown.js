d922a4938b9a255829c5610392eea5a766b89f2f
aq032 <anthonyq032@gmail.com>
2022-11-28 10:09:58 -0800

    Solved countdown algo

--- 04-Web-APIs/02-countdown/Unsolved/countdown.js
+++ 04-Web-APIs/02-countdown/Unsolved/countdown.js
@@ -1,4 +1,12 @@
 // Write code to print all numbers from `num` down to 1
 // You may assume `num` will be a positive number
 
-var countdown = function(num) {};
+var countdown = function (num) {
+
+  //console.logs num and then decrements it every time until it is less than or equal to 1
+  while (num >= 1) {
+    console.log(num);
+    num--;
+  }
+
+};
