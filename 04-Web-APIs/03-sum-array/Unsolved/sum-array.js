a4307411fd1fb2887ea3381ef8b8e5fd60e93a1a
aq032 <anthonyq032@gmail.com>
2022-12-02 19:55:59 -0800

    Solved 03-sum-array

--- 04-Web-APIs/03-sum-array/Unsolved/sum-array.js
+++ 04-Web-APIs/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,8 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function (arr) {
+  // Reduces a single array into one single number.
+  // Reducer requires two values, an accumulator and a current value.
+  // We simply add every value to the sum and we set our initial value to 0.
+  return arr.reduce((sum, currentValue) => sum + currentValue, 0);
+};
