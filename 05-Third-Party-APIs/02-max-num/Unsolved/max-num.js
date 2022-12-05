8bbb46e4a09939eaebe459b14f48ba3b25c39ec3
aq032 <anthonyq032@gmail.com>
2022-12-05 15:23:34 -0800

    Solved 02-max-num

--- 05-Third-Party-APIs/02-max-num/Unsolved/max-num.js
+++ 05-Third-Party-APIs/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,15 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function (arr) {
+  let max = arr[0];
+
+  // Loops through every array and compares it to the largest number thats been passed so far
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+  }
+
+  return max;
+};
